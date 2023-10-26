402d1d992edbad518f6c126dbf30e28ce9c1625c
Lxura27 <diazsuarezlaurageraldinne27@gmail.com>
2023-10-26 18:09:13 -0500

    Win

---
+++ Assets/Scripts/UI/Win.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Win : MonoBehaviour
+{
+    public void NextLevel()
+    {
+        SceneManager.LoadScene("Level");
+    }
+
+    public void Restart()
+    {
+       
+        SceneManager.LoadScene("Level");
+
+    }
+
+
+}
