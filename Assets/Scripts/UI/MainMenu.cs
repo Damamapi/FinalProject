57425c2a9a732b768f7f0cda4aceef8d89cb89be
Damamapi <ddamamapi@gmail.com>
2023-10-27 15:53:48 -0500

    Icons

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,24 +6,57 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public float fadeTime = 1.0f;
 
     public CanvasGroup mainMenu;
     public CanvasGroup settingsGroup;
+
+    private void Start()
+    {
+       StartCoroutine(StartDelay());
+    }
+
+    IEnumerator StartDelay()
+    {
+        yield return new WaitForSeconds(2);
+        StartCoroutine(FadeIn(mainMenu));
+    }
+
     public void Play()
     {
         SceneManager.LoadScene("Level 0");
     }
 
+    IEnumerator FadeOutThenIn(CanvasGroup outGroup, CanvasGroup inGroup)
+    {
+        yield return FadeOut(outGroup);
+        yield return FadeIn(inGroup);
+    }
+
+    IEnumerator FadeOut(CanvasGroup group)
+    {
+        group.alpha = 1f;
+        Tween tween = group.DOFade(0, fadeTime).SetEase(Ease.OutCubic);
+        yield return tween.WaitForCompletion();
+        group.gameObject.SetActive(false);
+    }
+
+    IEnumerator FadeIn(CanvasGroup group)
+    {
+        group.alpha = 0f;
+        group.gameObject.SetActive(true);
+        Tween tween = group.DOFade(1, fadeTime).SetEase(Ease.InCubic);
+        yield return tween.WaitForCompletion();
+    }
+
     public void Settings()
     {
-        mainMenu.gameObject.SetActive(false);
-        settingsGroup.gameObject.SetActive(true);
+        StartCoroutine(FadeOutThenIn(mainMenu, settingsGroup));
     }
 
     public void BackToMainMenu()
     {
-        mainMenu.gameObject.SetActive(true);
-        settingsGroup.gameObject.SetActive(false);
+        StartCoroutine(FadeOutThenIn(settingsGroup, mainMenu));
     }
 
     public void Credit()
