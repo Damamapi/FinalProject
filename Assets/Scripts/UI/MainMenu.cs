ff7c19ac33b5c395c5390d3b79b83e7db6dbeafc
Damamapi <ddamamapi@gmail.com>
2023-10-27 14:17:26 -0500

    MainMenuEdits

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -5,18 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-   public void Play()
+
+    public CanvasGroup mainMenu;
+    public CanvasGroup settingsGroup;
+    public void Play()
     {
         SceneManager.LoadScene("Level 0");
     }
 
     public void Settings()
     {
-        SceneManager.LoadScene("Setting");
+        mainMenu.gameObject.SetActive(false);
+        settingsGroup.gameObject.SetActive(true);
+    }
+
+    public void BackToMainMenu()
+    {
+        mainMenu.gameObject.SetActive(true);
+        settingsGroup.gameObject.SetActive(false);
     }
 
     public void Credit()
     {
-        SceneManager.LoadScene("Credits");
+        //SceneManager.LoadScene("Credits");
     }
 }
