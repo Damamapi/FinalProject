332e09c2ab2e30e732c78b982177199b18aa3fa8
Damamapi <ddamamapi@gmail.com>
2023-10-28 13:12:42 -0500

    Organze folders

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenu;
+    [SerializeField] private GameObject PauseButton;
+    private bool GamePaused = false;
+    public string restarLevel;
+
+    private void Update()
+    {
+        if (InputControl.IsInputAllowed && Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GamePaused) Resume();
+            else PauseUI();
+
+        }
+    }
+
+
+    public void PauseUI()
+    {
+        GamePaused = true;
+        pauseMenu.SetActive(true);
+        PauseButton.SetActive(false);
+        Time.timeScale = 0f;
+
+
+    }
+    public void Resume()
+    {
+        GamePaused = false;
+        Time.timeScale = 1f;
+        PauseButton.SetActive(true);
+        pauseMenu.SetActive(false);
+
+    }
+    public void Restart()
+    {
+        GamePaused = false;
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(restarLevel);
+
+    }
+
+    public void MainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+}
