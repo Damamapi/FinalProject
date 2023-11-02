483f43babe69857f3b88a034a4491fde8d5672f2
Lxura27 <diazsuarezlaurageraldinne27@gmail.com>
2023-11-02 16:47:26 -0500

    Win canvas change and fixed the collider of the tutorial

--- Assets/Scripts/UI/TutorialCollider.cs
+++ Assets/Scripts/UI/TutorialCollider.cs
@@ -1,22 +1,35 @@
 using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TutorialCollider : MonoBehaviour
 {
     public CanvasGroup textRotate;
+    public Transform player;
 
-    private void OnTriggerEnter(Collider other)
+    public void Update()
     {
-        if (other.CompareTag("Player"))
+        if (Vector3.Distance(player.position, transform.position) < 1)
         {
-           
-            textRotate.alpha = 1f;
-            textRotate.gameObject.SetActive(true);
-            textRotate.DOFade(1, 1f).SetEase(Ease.InCubic);
+            ActivateText();
+            gameObject.SetActive(false);
+
         }
-   
 
     }
+ 
+    private void ActivateText()
+    {
+        textRotate.alpha = 0f;
+        textRotate.gameObject.SetActive(true);
+        textRotate.DOFade(1, 1f).SetEase(Ease.InCubic);
+      
+    }
+
+   
+
+
 }
